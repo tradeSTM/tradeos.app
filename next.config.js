7fd5134b2ffd713a60c125740ac6442ca41f593f
GXQ STUDIO <144380926+SMSDAO@users.noreply.github.com>
2025-07-25 22:43:07 -0700

    feat: update GitHub Actions workflow for deployment; enhance Next.js configuration and package scripts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+    domains: [
+      'raw.githubusercontent.com',
+      'github.com',
+      'assets.coingecko.com'
+    ]
+  },
   output: 'export',
-  distDir: 'docs',
   trailingSlash: true,
 };
 
